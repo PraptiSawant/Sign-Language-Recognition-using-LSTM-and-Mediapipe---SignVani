d91dd9fcd222eb018642757bf0c7acaf326114b0
shrusti333 <naikgaonkarshrusti@gmail.com>
2023-06-10 15:57:20 +0530

    index and sample file

--- Gesture-Recognition-for-Sign-Language-Translation-main/index.js
+++ Gesture-Recognition-for-Sign-Language-Translation-main/index.js
@@ -8,17 +8,15 @@ app.set('view engine', 'html');
 app.use(express.static(__dirname + "/public"))
 
 
-app.get('/sample', function (req, res) {
+app.use('/', function (req, res) {
     // var model = loadModel();
-    request('http://127.0.0.1:5000/translate_to_Konkani', function (error, response, body) {
-        console.error('error:', error); // Print the error
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the data received
-        res.send(body); //Display the response on the website
-      });    
+    res.render(__dirname + '/public/sample.html', {sample: "Text"});
 });
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+
+
 
